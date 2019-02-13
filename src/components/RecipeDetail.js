715c9033b794f3177a1fc7f9469d30f912d931ae
sizin <sacar@excilys.com>
2019-02-13 17:24:48 +0100

    Adding + editing

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,24 +1,85 @@
-import React, {Component} from 'react';
-
-import {Card, CardBody, CardText, CardTitle, CardSubtitle, CardImg, Col, Button} from 'reactstrap';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Card, CardBody, CardText, CardTitle, Input, CardImg, Col, Row, Button } from 'reactstrap';
 
 class RecipeDetail extends Component{
+    state = {
+        recipe: this.props.recipe,
+        editMode: this.props.recipe ? false : true,
+        addMode: false
+    }
+
+    toggle = () => {
+        this.state.editMode && this.props.edit(this.state.recipe);
+        this.setState({editMode: !this.state.editMode})
+
+    }
+
+
+    submitNewRecipe = () => {
+        this.state.addMode && this.props.add(this.state.recipe);
+        this.setState({addMode: !this.state.addMode})
+
+    }
+
+    handleEditName = (event) => {
+        this.setState({recipe: {...this.state.recipe, name: event.target.value}});
+    }
+
+    handleEditPicture = (event) => {
+        this.setState({recipe: {...this.state.recipe, picture: event.target.value}})
+    }
+
+    handleEditDescription = (event) => {
+        this.setState({recipe: {...this.state.recipe, description: event.target.value}})
+    }
+
+    
     render(){
         //Destructration ! LEs noms des arguments doivent matcher ceur de recipe
         // let {name, picture, description} = this.props.recipe;
-        let {name: otherName, picture, description} = this.props.recipe;
+        // let {name: otherName, picture, description} = this.state.recipe;
         return (
-            <Col md={4}>
-                <Card>
-                    <CardImg src={picture}></CardImg>
-                    <CardBody>
-                        <CardTitle>{otherName}</CardTitle>
-                        <CardSubtitle>{description}</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
-            </Col>
+                <Col md={4}>
+                    <Card>
+                        <CardBody>
+                            <Row>
+                                <Col>
+                                    {
+                                        this.state.editMode ? 
+                                        <Input defaultValue={this.state.recipe && this.state.recipe.name} onChange={this.handleEditName} /> : 
+                                        <CardTitle>{this.state.recipe.name}</CardTitle>
+                                    }
+                                </Col>
+                                <FontAwesomeIcon icon={faEdit} onClick={this.toggle}/>
+                                <FontAwesomeIcon icon={faTrash} onClick={this.props.delete}/>
+                            </Row>
+                        </CardBody>
+
+                        <CardBody>
+                            {
+                                this.state.editMode ?
+                                <Input defaultValue={this.state.recipe && this.state.recipe.picture} onChange={this.handleEditPicture} /> :
+                                <CardImg src={this.state.recipe.picture}></CardImg>
+                            }
+                        </CardBody>
+
+
+                        <CardBody>
+                            {
+                                this.state.editMode ?
+                                <Input defaultValue={this.state.recipe && this.state.recipe.description} onChange={this.handleEditDescription} /> :
+                                <CardText>{this.state.recipe.description}</CardText>
+                            }
+                            
+                        </CardBody>
+                        <CardBody>
+                         { !this.props.recipe && <Button onClick={this.submitNewRecipe}>Submit</Button> }
+                        </CardBody>
+                    </Card>
+                </Col>
         )
     }
 }
--- src/containers/RecipeList.js
+++ src/containers/RecipeList.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
 import RecipeDetail from '../components/RecipeDetail.js';
-import {Row, Container} from 'reactstrap';
+import {Row, Container, Button} from 'reactstrap';
 //Pour importer MOCK il faut utiliser des accolades car c'est un import nommé et non par défaut
 import {MOCK} from '../Mock.js';
 
 
-class RecipeList extends Component {
-  render() {
 
-    // Le return est forcement qu'UN seul composant avec des sous composants
-    return (
-      <div className="RecipeList">
-        <Container>
-          <Row>
-            {MOCK.map((recipe) => 
-              <RecipeDetail recipe={recipe} key={recipe.id}/>
-                )}
-            
-          </Row>
-        </Container>
-      </div>
-    );
+class RecipeList extends Component {
+    state={
+        data: MOCK,
+        addMode: false
+    }
+
+    // Delete fonction, double method
+    // Il faut deux niveau de méthode car par défault React appelera la fonction si elle ne possède pas un second niveau
+    delete = (id) => () => {
+        this.setState({data: this.state.data.filter(recipe => recipe.id !== id)})
+    }
+
+    edit = (recipe) => {
+        var temp = this.state.data.filter(item => recipe.id !== item.id)
+        temp.push(recipe)
+        this.setState({data: temp})
+    }
+
+    add = (newRecipe) => {
+
+        console.log(newRecipe);
+        var temp = this.state.data;
+        temp.push(newRecipe);
+        this.setState({data: temp})
+    }
+
+
+    toggleAddRecipe = () => {
+        // this.state.addMode && this.props.add(this.state.recipe);
+        this.setState({addMode: !this.state.addMode})
+    }
+
+    render() {
+        let {data} = this.state;
+
+        // Le return est forcement qu'UN seul composant avec des sous composants
+        return (
+        <div className="RecipeList">
+            <Container>
+            <Row>
+
+                {/* map() est une boucle */}
+
+                {data.map((recipe) => 
+                <RecipeDetail recipe={recipe} key={recipe.id} edit={this.edit} delete={this.delete(recipe.id)}/>
+                    )}
+
+                {/* Alors içi on donne 'delete' en props, qui sera utilisé dans l'enfant RecipeDetail */}
+
+                {  
+                    // If addmode is true then we display recipeDetail card but with empty value
+                    this.state.addMode && <RecipeDetail edit={this.edit} delete={this.delete} add={this.add}></RecipeDetail> 
+                }
+
+            </Row>
+
+                <Button onClick={this.toggleAddRecipe}> Add </Button> 
+
+            </Container>
+        </div>
+        );
   }
 }
 
